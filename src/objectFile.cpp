2af81812b2a30487c32da8be2b4ad64294cbb4e3
Aleksandar <pa200059d@student.etf.bg.ac.rs>
2023-12-28 18:56:44 +0100

    ObjectFile class : read sections from file

--- src/objectFile.cpp
+++ src/objectFile.cpp
@@ -23,5 +23,24 @@ void ObjectFile::populateSymbolTable() {
 }
 
 void ObjectFile::populateSectionTable() {
-
+	uint8_t* sectionData;
+	relData rte;
+	sectionTableEntry ste;
+	Section* sec;
+	for(uint32_t i = 0 ; i < mh.SECTION_TABLE_LENGTH ; i++) {
+		fseek(file, mh.SECTION_TABLE_OFFSET+i*sizeof(sectionTableEntry), SEEK_SET);
+		fread(&ste, sizeof(sectionTableEntry), 1, file);
+		sec = new Section(std::string(stringPool+ste.SECTION_NAME_OFFSET));
+		sec->setSize(ste.SECTION_SIZE);
+		sectionData = new uint8_t[ste.SECTION_SIZE];
+		fseek(file, ste.SECTION_DATA_OFFSET, SEEK_SET);
+		fread(sectionData, sizeof(uint8_t), ste.SECTION_SIZE, file);
+		sec->putData(sectionData, ste.SECTION_SIZE);
+		fseek(file, ste.REL_DATA_TABLE_OFFSET, SEEK_SET);
+		for(uint32_t j = 0 ; j < ste.REL_DATA_TABLE_LENGTH ; j++) {
+			fread(&rte, sizeof(relData), 1, file);
+			sec->getRelocationTable().push_back(rte);
+		}
+		delete[] sectionData;
+	}
 }
