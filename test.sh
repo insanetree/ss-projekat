d246e2a5b712ed88b0043f7d3b18a154de4c291f
Aleksandar <pa200059d@student.etf.bg.ac.rs>
2024-01-16 04:37:18 +0100

    idk

--- test.sh
+++ test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-make all -j8
-
-./build/assembler -o handler.o tests/test1/handler.s
-./build/assembler -o main.o tests/test1/main.s
-./build/linker -relocatable -o output.o handler.o main.o
-./build/linker -hex -place=my_code_main@0x40000000 -place=my_code_handler@0xc0000000 -o program.hex handler.o main.o
-./build/emulator program.hex
+assembler -o handler.o tests/test1/handler.s
+assembler -o main.o tests/test1/main.s
+linker -relocatable -o output.o handler.o main.o
+linker -hex -place=my_code_main@0x40000000 -place=my_code_handler@0xc0000000 -o program.hex handler.o main.o
+emulator program.hex
 
